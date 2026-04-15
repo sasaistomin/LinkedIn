55dc7d8e275a34acf75151e926627afd1b774ffd
sasasasasasaasasassa <sasaistomin42@gmail.com>
2026-04-15 10:41:44 +0300

    12

--- linkedin/src/component/buttonRegistrate/buttonRegistrate.js
+++
@@ -1,9 +0,0 @@
-import './buttonRegistrate.css';
-
-const buttonRegistrate = () => (
-  <div className="buttonRegistrate">
-    buttonRegistrate Component
-  </div>
-);
-
-export default buttonRegistrate;
---
+++ linkedin/src/component/secondSetion/secondSetion.jsx
@@ -0,0 +1,14 @@
+import './secondSetion.css';
+
+const SecondSetion = (props) => (
+    <div className="secondSetion">
+        <div className='textLeft'>
+            <h2>{props.text}</h2>
+        </div>
+        <div className='listRight'>
+            <h1>sddsds</h1>
+        </div>
+    </div>
+);
+
+export default SecondSetion;
