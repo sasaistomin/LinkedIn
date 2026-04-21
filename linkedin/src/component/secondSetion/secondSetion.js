557f7fd28f35e5bc788aad914d41b08d6e1bc05e
Desokir <ska866234@gmail.com>
2026-04-21 09:35:51 +0300

    Update and rename secondSetion.jsx to secondSetion.js

---
+++ linkedin/src/component/secondSetion/secondSetion.js
@@ -0,0 +1,25 @@
+import './secondSetion.css';
+import { Link } from 'react-router-dom';
+
+const List = ({ item }) => {
+    return (
+        <Link to='login'>
+            {item.map((word, index) => (
+                <div key={index} className='chip'>{word}</div>
+            ))}
+        </Link>
+    );
+};
+
+const SecondSetion = (props) => (
+    <div className="secondSetion">
+        <div className='textLeft'>
+            <h2 className='text'>{props.text}</h2>
+        </div>
+        <div className='listRight'>
+            <List item={props.item}/>
+        </div>
+    </div>
+);
+
+export default SecondSetion;
